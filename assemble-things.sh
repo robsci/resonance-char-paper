d63933ecda5ff7799eecfcb46dfe645abb5f01d1
Christopher Berry <christopher.berry@ligo.org>
2017-01-16 19:17:26 +0000

    PnP comments

--- assemble-things.sh
+++ assemble-things.sh
@@ -10,25 +10,46 @@ cp res_char.bbl ./submit/
 
 cp Resonances.bib ./submit/
 
-cp Fig_good_traj.* ./submit/
-cp Fig_overlap.* ./submit/
-cp Fig_res_diff_traj.* ./submit/
-cp Fig_res_jump_calc.* ./submit/
-cp Fig_q_jump.* ./submit/
-cp Fig_e_rms_p.* ./submit/
-cp Fig_nu_rms_p.* ./submit/
-cp Fig_res_pop_M.* ./submit/
-cp Fig_res_pop_a.* ./submit/
-cp Fig_res_pop_z.* ./submit/
-cp Fig_res_pop_p.* ./submit/
-cp Fig_res_pop_e.* ./submit/
-cp Fig_res_pop_iota.* ./submit/
-cp Fig_res_pop_t.* ./submit/
-cp Fig_res_pop_SNR.* ./submit/
-cp Fig_res_overlap_t.* ./submit/
-cp Fig_res_pop_SNR_overlap.* ./submit/
-cp Fig_res-2-5-95-plane.* ./submit/
-cp Fig_fit-error-max-plane.* ./submit/
-cp Fig_fit-error-RMS-plane.* ./submit/
+#cp Fig_good_traj.pdf ./submit/
+#cp Fig_overlap.pdf ./submit/
+#cp Fig_res_diff_traj.pdf ./submit/
+#cp Fig_res_jump_calc.pdf ./submit/
+#cp Fig_q_jump.pdf ./submit/
+#cp Fig_e_rms_p.pdf ./submit/
+#cp Fig_nu_rms_p.pdf ./submit/
+#cp Fig_res_pop_M.pdf ./submit/
+#cp Fig_res_pop_a.pdf ./submit/
+#cp Fig_res_pop_z.pdf ./submit/
+#cp Fig_res_pop_p.pdf ./submit/
+#cp Fig_res_pop_e.pdf ./submit/
+#cp Fig_res_pop_iota.pdf ./submit/
+#cp Fig_res_pop_t.pdf ./submit/
+#cp Fig_res_pop_SNR.pdf ./submit/
+#cp Fig_res_overlap_t.pdf ./submit/
+#cp Fig_res_pop_SNR_overlap.pdf ./submit/
+#cp Fig_res-2-5-95-plane.pdf ./submit/
+#cp Fig_fit-error-max-plane.pdf ./submit/
+#cp Fig_fit-error-RMS-plane.pdf ./submit/
+
+cp Fig_good_traj.pdf ./submit/
+cp Fig_overlap.pdf ./submit/
+cp Fig_res_diff_traj.pdf ./submit/
+cp Fig_res_jump_calc.pdf ./submit/
+cp Fig_q_jump.pdf ./submit/
+cp Fig_e_rms_p.pdf ./submit/
+cp Fig_nu_rms_p.pdf ./submit/
+cp Fig_res_pop_M.pdf ./submit/
+cp Fig_res_pop_a.pdf ./submit/
+cp Fig_res_pop_z.pdf ./submit/
+cp Fig_res_pop_p.pdf ./submit/
+cp Fig_res_pop_e.pdf ./submit/
+cp Fig_res_pop_iota.pdf ./submit/
+cp Fig_res_pop_t.pdf ./submit/
+cp Fig_res_pop_SNR.pdf ./submit/
+cp Fig_res_overlap_t.pdf ./submit/
+cp Fig_res_pop_SNR_overlap.pdf ./submit/
+cp Fig_res-2-5-95-plane.pdf ./submit/
+cp Fig_fit-error-max-plane.pdf ./submit/
+cp Fig_fit-error-RMS-plane.pdf ./submit/
 
 tar -zcvf res-char.tar.gz submit
